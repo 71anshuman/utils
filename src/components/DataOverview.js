7d2cecabd1f63a5ef3806ade5fa9605fab75e751
Anshuman lawania <71anshuman@gmail.com>
2021-04-13 16:06:06 +0530

    chart view logic updated

--- src/components/DataOverview.js
+++ src/components/DataOverview.js
@@ -6,24 +6,26 @@ export default function DataOverview({meta}) {
     return (
         <div className="col-md-12">
             <div className="jumbotron jumbotron-fluid bg-light border-bottom border-grey">
-                <div className="container row">
-                    <div className="col-md-4">
-                        <h4 className="display-6">
-                            { numberFormat(investmentAmount) }<br/>
-                            <small className="text-muted">INVESTED</small>
-                        </h4>
-                    </div>
-                    <div className="col-md-4">
-                        <h4 className="display-6">
-                            { numberFormat(interestAmount) }<br/>
-                            <small className="text-muted">GAIN</small>
-                        </h4>
-                    </div>
-                    <div className="col-md-4">
-                        <h4 className="display-6">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h4>
+                                { numberFormat(investmentAmount) }<br/>
+                                <small className="text-muted">INVESTED</small>
+                            </h4>
+                        </div>
+                        <div className="col-md-4">
+                            <h4>
+                                { numberFormat(interestAmount) }<br/>
+                                <small className="text-muted">GAIN</small>
+                            </h4>
+                        </div>
+                        <div className="col-md-4">
+                        <h4>
                             { numberFormat(finalBalance) }<br/>
                             <small className="text-muted text-uppercase">maturity value</small>
                         </h4>
+                    </div>
                     </div>
                 </div>
             </div>
--- src/components/InvestedVsGainChart.js
+++ src/components/InvestedVsGainChart.js
@@ -5,22 +5,24 @@ export default function InvestedVsGainChart(props) {
     const { investmentAmount, interestAmount } = meta;
     return (
         <div>
-            <Chart
-                width={'100%'}
-                height={'400px'}
-                chartType="PieChart"
-                loader={<div>Loading Chart</div>}
-                data={[
-                    ['Amount', 'INR'],
-                    ['Invested', investmentAmount],
-                    ['Gain', interestAmount],
-                ]}
-                options={{
-                    title: 'Gain VS Invested',
-                    is3D: true,
-                }}
-                rootProps={{ 'data-testid': '2' }}
+            {investmentAmount !== 0 &&
+                <Chart
+                    width={'100%'}
+                    height={'400px'}
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                        ['Amount', 'INR'],
+                        ['Invested', investmentAmount],
+                        ['Gain', interestAmount],
+                    ]}
+                    options={{
+                        title: 'Gain VS Invested',
+                        is3D: true,
+                    }}
+                    rootProps={{ 'data-testid': '2' }}
                 />
+            }
         </div>
     )
 }
